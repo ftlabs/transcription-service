eadd51b2dcfb9b0cb02ed9add76cba53c1d412e5
Sean Tracey <seanmtracey@gmail.com>
2016-12-15 16:23:14 +0000

    Check if file type is valid.

--- bin/lib/valid-file.js
+++ bin/lib/valid-file.js
@@ -3,5 +3,8 @@ const filetype = require('file-type');
 const validFileTypes = process.env.VALID_FILE_TYPES ? process.env.VALID_FILE_TYPES.split(',') : ['mp4', 'wav', 'mp3', 'ogg', 'm4a'];
 
 module.exports = function(buff){
-	return filetype(buff);
+
+	const fileInfo = filetype(buff);
+	return fileInfo !== null ? validFileTypes.indexOf( fileInfo.ext ) > -1 : false;
+
 }
