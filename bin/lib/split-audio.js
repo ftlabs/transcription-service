530258184f5cd74ac349c660cfcc40bb16a570b1
Sean Tracey <seanmtracey@gmail.com>
2017-01-27 10:53:34 +0000

    Abstract ffmpeg job

--- bin/lib/split-audio.js
+++ bin/lib/split-audio.js
@@ -6,6 +6,41 @@ const shortID = require('shortid').generate;
 
 const tmpPath = process.env.TMP_PATH || '/tmp';
 
+function runFFmpeg(args){
+
+	return new Promise( (resolve, reject) => {
+
+		let output = '';
+		const process = spawn(ffmpeg.path, args);
+
+		process.stdout.on('data', (data) => {
+			if(process.env.VERBOSE_FFMPEG){
+				debug(`stdout: ${data}`);
+			}
+			output += data + '\n';
+		});
+
+		process.stderr.on('data', (data) => {
+			debug(`stderr: ${data}`);
+		});
+
+		process.on('close', (code) => {
+
+			if(code === 1){
+				debug(`FFMPEG exited with status code 1 while converting ${sourceFilePath} to OGG`);
+				reject();
+			} else if(code === 0){
+				debug('FFMPEG closed and was happy');
+				resolve(output);
+			}
+
+		});
+
+	});
+
+
+}
+
 // ffmpeg -i somefile.mp3 -f segment -segment_time 3 -c copy out%03d.mp3
 module.exports = function(sourceFilePath, jobID, duration = 3){
 
@@ -17,7 +52,8 @@ module.exports = function(sourceFilePath, jobID, duration = 3){
 		fs.mkdir(splitFilesDestination, function(err){
 
 			debug(err);
-
+			
+			//ffmpeg -i /Users/sean.tracey/Downloads/1214e988-b6d7-11e6-ba85-95d1533d9a62.mp3  -af silencedetect=n=-40dB:d=0.2 -f null -
 			const args = [
 				'-i',
 				sourceFilePath,
@@ -30,35 +66,22 @@ module.exports = function(sourceFilePath, jobID, duration = 3){
 				`${splitFilesDestination}/out%03d.wav`
 			];
 			debug(`Splitting files in ${tmpPath}/${jobID}/`);
-			const process = spawn(ffmpeg.path, args);
 			
-			process.stdout.on('data', (data) => {
-				if(process.env.VERBOSE_FFMPEG){
-					debug(`stdout: ${data}`);
-				}
-			});
-
-			process.stderr.on('data', (data) => {
-				debug(`stderr: ${data}`);
-			});
-
-			process.on('close', (code) => {
-
-				if(code === 1){
-					debug(`FFMPEG exited with status code 1 while converting ${sourceFilePath} to OGG`);
-					reject();
-				} else if(code === 0){
-					debug('FFMPEG closed and was happy');
+			runFFmpeg(args)
+				.then(function(){
 					fs.readdir(splitFilesDestination, (err, files) => {
 						if(err){
 							reject(err);
 						} else {
 							resolve(files.map(f => {return `${splitFilesDestination}/${f}`;}));
 						}
-					})
-				}
-
-			});
+					});
+				})
+				.catch(err => {
+					debug(`An error occurred splitting the audio`);
+					reject(err);
+				})
+			;
 
 		});
 
