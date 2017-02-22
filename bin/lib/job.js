3a49e9d49c74e05b79d358ba63c8e6f7c2618ffa
Sean Tracey <seanmtracey@gmail.com>
2017-02-22 10:09:47 +0000

    Oh, go on then, let's spoil ourselves and use ES6 classes...

--- bin/lib/job.js
+++ bin/lib/job.js
@@ -9,105 +9,109 @@ const getTimeIndexes = require('./generate-time-indexes');
 
 const tmpPath = process.env.TMP_PATH || '/tmp';
 
-function Job(id, language){
-	this.id = id;
-	this.language = language;
-	this.finished = false;
-	this.inProgress = false;
-	this.transcription = undefined;
-}
-
-Job.prototype.start = function(){
+class Job {
 	
-	bucket.get(this.id)
-		.then(data => {
+	constructor(id, language) {
+		this.id = id;
+		this.language = language;
+		this.finished = false;
+		this.inProgress = false;
+		this.transcription = undefined;
+	}
 
-			const destination = `${tmpPath}/${this.id}`;
+	start(){
 
-			return new Promise( (resolve, reject) => {
-					fs.writeFile(destination, data.Body, err => {
-						if(err){
-							reject(err);
-						} else {
-							resolve(`${tmpPath}/${this.id}`);
-						}
+		bucket.get(this.id)
+			.then(data => {
+
+				const destination = `${tmpPath}/${this.id}`;
+
+				return new Promise( (resolve, reject) => {
+						fs.writeFile(destination, data.Body, err => {
+							if(err){
+								reject(err);
+							} else {
+								resolve(`${tmpPath}/${this.id}`);
+							}
+						})
 					})
-				})
-				.catch(err => {
-					debug(err);
-					this.failed = true;
-				})
-			;
+					.catch(err => {
+						debug(err);
+						this.failed = true;
+					})
+				;
 
-		})
-		.then(filePath => {
-			prepareAudio(filePath, this.id, process.env.AUDIO_MAX_DURATION_TIME || 55)
-				// Get a transcription of the whole audio to serve as a guide for the chunks
-				.then(audio => transcribeAudio(audio, undefined, this.language))
-				.then(transcriptions => {
-					debug('Whole transcriptions:', transcriptions);
+			})
+			.then(filePath => {
+				prepareAudio(filePath, this.id, process.env.AUDIO_MAX_DURATION_TIME || 55)
+					// Get a transcription of the whole audio to serve as a guide for the chunks
+					.then(audio => transcribeAudio(audio, undefined, this.language))
+					.then(transcriptions => {
+						debug('Whole transcriptions:', transcriptions);
 
-					if(transcriptions.length > 1){
-						transcriptions = transcriptions.join(' ');
-					} else {
-						transcriptions = transcriptions[0];
-					}
+						if(transcriptions.length > 1){
+							transcriptions = transcriptions.join(' ');
+						} else {
+							transcriptions = transcriptions[0];
+						}
 
-					// Split the audio file into 3 second chunks for time-based transcriptions
-					return prepareAudio(filePath, this.id)
-						.then(files => {
-							// Get the time indexes + offsets for each audio chunk
-							return getTimeIndexes(files)
-								.then(durations => {
-									return {
-										files,
-										durations
-									};
-								})
-							;
-
-						})
-						.then(data => {
-							// Transcribe all of the smaller audio chunks
-							return transcribeAudio(data.files, transcriptions, this.language)
-								.then(transcriptions => {
-									// Link up the small audio transcriptions with the 
-									// time indexes of each file
-									return transcriptions.map( (t, idx) => {
+						// Split the audio file into 3 second chunks for time-based transcriptions
+						return prepareAudio(filePath, this.id)
+							.then(files => {
+								// Get the time indexes + offsets for each audio chunk
+								return getTimeIndexes(files)
+									.then(durations => {
 										return {
-											transcription : t,
-											timeOffsets : data.durations[idx]
+											files,
+											durations
 										};
-									});
-								})
-								.then(transcribedChunks => { 
-									return {
-										whole : transcriptions,
-										transcribedChunks
-									};
-								})
-							;
-						})
-					;
+									})
+								;
 
-				})
-				.then(transcriptions => {
+							})
+							.then(data => {
+								// Transcribe all of the smaller audio chunks
+								return transcribeAudio(data.files, transcriptions, this.language)
+									.then(transcriptions => {
+										// Link up the small audio transcriptions with the 
+										// time indexes of each file
+										return transcriptions.map( (t, idx) => {
+											return {
+												transcription : t,
+												timeOffsets : data.durations[idx]
+											};
+										});
+									})
+									.then(transcribedChunks => { 
+										return {
+											whole : transcriptions,
+											transcribedChunks
+										};
+									})
+								;
+							})
+						;
 
-					this.transcription = transcriptions;
-					this.finished = true;
-					cleanUp(this.id);
+					})
+					.then(transcriptions => {
 
-				})
-			;
+						this.transcription = transcriptions;
+						this.finished = true;
+						cleanUp(this.id);
 
-		})
-		.catch(err => {
-			debug(err);
-			cleanUp(this.id);
-			this.failed = true;
-		})
-	;
+					})
+				;
 
-};
+			})
+			.catch(err => {
+				debug(err);
+				cleanUp(this.id);
+				this.failed = true;
+			})
+		;
+
+	}
+
+}
 
 module.exports = Job;
